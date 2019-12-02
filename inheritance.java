259177fb61bc49153d3abef5e61aaf252ca59b37
SUMAL-JPG <58158640+SUMAL-JPG@users.noreply.github.com>
2019-12-02 14:48:29 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,24 @@
+package corejava;
+
+class p {
+	 void add(int a,int b)
+	 {
+		 int s=a+b+10;
+		 System.out.println(s);
+	 }
+	}
+	class c extends p{
+		void add(int a,int b)
+		{
+			int s=a+b;
+			System.out.println(s);
+		}
+	 }
+	public class inheritance {
+	public static void main(String[] args) {
+	     p obj=new p();
+	     obj.add(10,20);
+	}
+	}
+
+
