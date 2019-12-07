7fa9db529afc9ee8f3d0f338bc3278ed42305b8c
SUMAL-JPG <58158640+SUMAL-JPG@users.noreply.github.com>
2019-12-07 14:56:25 +0530

    Add files via upload

---
+++ Staticexample.java
@@ -0,0 +1,19 @@
+package Innerclass;
+
+ class Staticexample {
+static int count=0;
+	 Staticexample()
+
+{
+count++;
+System.out.println(count);
+
+}
+	public static void main(String[] args) {
+		Staticexample c1=new Staticexample();		 
+		Staticexample c2=new Staticexample();		 
+		Staticexample c3=new Staticexample();		 
+
+	}
+
+}
